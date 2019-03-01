53ccbfc6b71d23b8264f7bbb135f9fcf1f634d99
ben Petersen <ben83pete@gmail.com>
2019-03-01 14:46:10 -0500

    working game

---
+++ js/java-dice.js
@@ -0,0 +1,34 @@
+
+
+function playGames(){
+    var idx = 0;
+    var highScore = 0;
+
+    while (idx < 100){
+        var gameScore = oneGame();
+        if(gameScore > highScore){
+            highScore = gameScore;
+        }
+        idx++;        
+    }
+    var scoreCtrl = document.getElementById("score");
+
+    scoreCtrl.value = highScore;
+}
+
+function oneGame(){
+    console.log("dice function called");
+    var total = 0;
+    var roll = rollTheDie();
+    while(roll !=1){
+
+        total += roll;
+
+        roll=rollTheDie();
+    }
+    return total;
+}
+
+function rollTheDie(){
+    return Math.floor(Math.random()*6 + 1);
+}
